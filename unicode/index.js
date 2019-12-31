e3b1b28ad7679f968ef61a6dd6ff9f5d3b926bc1
sgalal <32255369+chromezh@users.noreply.github.com>
2020-01-01 04:00:13 +0800

    Update index and implement unicode

---
+++ unicode/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var dictmap;
+
+fetch('https://nushuscript.org/unicode_nushu/map.json')
+.then(function(response) { return response.json(); })
+.then(function(res) {
+	dictmap = res;
+});
+
+function makeSingleOrMultipleEntry(ress) {
+	var span = document.createElement('span');
+	span.lang = 'zh-Nshu';
+	span.innerText = ress.map(function(res) {
+		return String.fromCodePoint(parseInt(res));
+	}).join('');
+	return span;
+}
+
+function handleConvert() {
+	document.getElementById('outputArea').innerHTML = '';
+
+	document.getElementById('inputArea').value.split('').map(function(ch) {
+		var ress = dictmap[ch];
+		if (!ress) {
+			document.getElementById('outputArea').appendChild(document.createTextNode(ch));
+		} else {
+			document.getElementById('outputArea').appendChild(document.createTextNode(ch));
+			document.getElementById('outputArea').appendChild(makeSingleOrMultipleEntry(ress));
+		}
+	});
+
+	document.getElementById('outputArea').focus();
+}
