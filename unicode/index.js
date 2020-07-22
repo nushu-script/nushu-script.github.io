8208d8977c8f98af5d4d100cfc032e53109bb144
sgalal <sgalal.me@outlook.com>
2020-07-22 20:17:24 +0800

    Unify Image/Text converter

--- unicode/index.js
+++ unicode/index.js
@@ -4,27 +4,58 @@ var dictmap;
 
 fetch('https://nushuscript.org/unicode_nushu/map.json')
 .then(function(response) { return response.json(); })
-.then(function(res) {
-	dictmap = res;
-});
+.then(function(res) { dictmap = res; });
 
-function makeSingleOrMultipleEntry(ress) {
+function nushuCodePoint2String(cp) {
+	return String.fromCodePoint(parseInt(cp));
+}
+
+function makeEntry_nsgfzsfzt(ress) {
+	return ress.map(function(res) {
+		var img = document.createElement('img');
+		img.src = 'https://cdn.jsdelivr.net/gh/nushu-script/nushu-nsgfzsfzt@latest/' + nushuCodePoint2String(res) + '.jpg';
+		return img;
+	});
+}
+
+function makeEntry_ntn(ress) {
 	var span = document.createElement('span');
 	span.lang = 'zh-Nshu';
-	span.innerText = ress.map(function(res) {
-		return String.fromCodePoint(parseInt(res));
-	}).join('');
+	span.classList.add('noto-traditional-nushu');
+	span.innerText = ress.map(nushuCodePoint2String).join('');
+	return span;
+}
+
+function makeEntry_un(ress) {
+	var span = document.createElement('span');
+	span.lang = 'zh-Nshu';
+	span.classList.add('unicode-nushu');
+	span.innerText = ress.map(nushuCodePoint2String).join('');
 	return span;
 }
 
 function handleConvert() {
 	document.getElementById('outputArea').innerHTML = '';
 
+	var styleSelectValue = styleSelect.options[styleSelect.selectedIndex].value;
+
 	document.getElementById('inputArea').value.split('').map(function(ch) {
 		document.getElementById('outputArea').appendChild(document.createTextNode(ch));
 		var ress = dictmap[ch];
 		if (ress) {
-			document.getElementById('outputArea').appendChild(makeSingleOrMultipleEntry(ress));
+			switch (styleSelectValue) {
+				case 'nsgfzsfzt':
+					makeEntry_nsgfzsfzt(ress).map(function(res) {
+						document.getElementById('outputArea').appendChild(res);
+					});
+					break;
+				case 'noto-traditional-nushu':
+					document.getElementById('outputArea').appendChild(makeEntry_ntn(ress));
+					break;
+				case 'unicode-nushu':
+					document.getElementById('outputArea').appendChild(makeEntry_un(ress));
+					break;
+			}
 		}
 	});
 
